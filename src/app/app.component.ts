ce213faa02e2e8f6e449d0f2df504f37b8bbf1c1
Markoradoj <G00436648@atu.ie>
2025-02-21 13:25:32 +0000

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'root-directive',
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+//To declare components 
 export class AppComponent {
   title = 'my-first-app';
+  name:string = 'Marko';
+  age:number = 23;
+
+  
 }
